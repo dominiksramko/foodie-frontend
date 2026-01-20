698dd182bd090a87b935f9d2edcae82a8ec9f3e9
Dominik Šramko <dominiksramko@gmail.com>
2026-01-20 22:12:40 +0100

    run first lint

---
+++ app/(tabs)/diary.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Diary() {
+  return (
+    <View>
+      <Text className="mx-2 my-1 bg-green-500 px-2 py-1 text-xl">Diary</Text>
+    </View>
+  );
+}
